2affa7d7ba4b04e4c90f5ade424d5b8c4ff6460f
SparksFullStack <csparks@antioch.edu>
2019-08-25 15:40:44 -0400

    first commit; user migrations set up

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <div class="container">
+        <h1>Post View</h1>
+        <br>
+        Post ID: <span>{{ $id }}</span>
+    </div>
+</body>
+</html>
